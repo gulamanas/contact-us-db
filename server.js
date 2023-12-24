7fb34d45d23e37f72ac7868d7da804a47b8b0924
Gulam Anas <g.anas786@gmail.com>
2023-12-25 00:55:27 +0530

    / routes

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/contacts', contactRoute);
+app.use('/', (req, res) => {
+  res.send('Welcome to the home page');
+});
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
